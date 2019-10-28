2018ad88dbb2ea762751a1340d5ec2b17585811f
chenli159 <2898296638@qq.com>
2019-10-28 16:40:54 +0800

    login页面静态完成

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -5,10 +5,18 @@ import Logo from "./images/logo.png"
 import "./login.scss"
 class Login extends Component {
     constructor(props) {
-        super(props);
-        this.state = {}
+        super(props)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleSubmit(e) {
+        e.preventDefault()
+        const form = this.props.form
+        const values = form.getFieldsValue()
+        console.log(values)
     }
     render() {
+        // const form = this.props.form
+        const { getFieldDecorator } = this.props.form
         return (
             <div className="login">
                 <div className="login-head">
@@ -18,29 +26,39 @@ class Login extends Component {
                 <div className="form">
                     <h3>用户登录</h3>
                     <div className="form-main">
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                登录
+                        <Form onSubmit={this.handleSubmit} className="login-form">
+                            <Form.Item>
+                                {getFieldDecorator('username', {
+                                    rules: [{ required: true, message: 'Please input your username!' }],
+                                })(
+                                    <Input
+                                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        placeholder="用户名"
+                                    />
+                                )}
+                            </Form.Item>
+                            <Form.Item>
+                                {getFieldDecorator('password', {
+                                    rules: [{ required: true, message: 'Please input your password!' }],
+                                })(
+                                    <Input
+                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        type="password"
+                                        placeholder="密码"
+                                    />
+                                )}
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                    登录
                             </Button>
-                        </Form.Item>
+                            </Form.Item>
+                        </Form>
                     </div>
                 </div>
             </div>
         );
     }
 }
-
-export default Login;
+const WrapLogin = Form.create()(Login)
+export default WrapLogin;
